e2cb0f6395e8ebfd95c12e1dadab83c5e6bd9a75
Michael Goolsby <mgoolsby1091@gmail.com>
2020-12-11 22:07:13 -0500

    removes lorem from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = (props) => {
     return <>
     <img src={bgImage} className="bg-img" alt="box with a bow and some holly"></img>
     <h3 className="home-page">Welcome to Box & Bow</h3>
-    <p className="home-page-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos laudantium maiores cumque sunt? Cum excepturi perferendis, incidunt perspiciatis ipsum quisquam ratione amet, voluptas, libero nemo aliquid id odit autem!</p>
+    <p className="home-page-p">Mom and Dad bugging you for that Holiday List? Grandma looking for something to get you? Having a hard time thinking of what you want this year? Start using Box & Bow to put together your holiday shopping lists, whether for yourself or your loved ones today! Sign up and log in to begin using! It's always free!</p>
     </>
 }
 
